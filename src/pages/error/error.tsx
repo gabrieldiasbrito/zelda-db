0c7085dbe88a1bbf49befa73943c6937e313f8e5
Gabriel Brito <67193804+gabrieldiasbrito@users.noreply.github.com>
2023-12-16 15:46:34 -0300

    Error page
    Criada a página de erro 404

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom';
+import { IoArrowBackOutline } from "react-icons/io5";
+import Link404 from '../../img/404-link.png'
+import './error.css'
+
 export default function Error(){
     return(
-        <h1>404</h1>
+        <div className=' page page-404'>
+            <div>
+                <h3>404</h3>
+                <br />
+                <h1>Página não encontrada</h1>
+                <p>Não foi possível encontrar a página que está procurando.</p>
+                <br />
+                <Link to="/" className='link-404'> <IoArrowBackOutline /> Voltar</Link>
+            </div>
+            <img src={Link404}/>
+        </div>
     )
 }
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,7 @@
 export default function Home(){
     return(
         <div className="page">
+            <h1>asdasd</h1>
         </div>
     )
 }
